c3a69b1965eaafd1e54363fb80a5f8fa067d514a
SnyDi611 <d-snyatkov@mail.ru>
2023-10-03 19:23:42 +0300

    Big UI improvements, added function to collect all table info

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,6 @@
 import { Handle, Position, VueFlow, useVueFlow } from '@vue-flow/core'
 import { Background, BackgroundVariant } from '@vue-flow/background'
 import { ref, provide, defineProps, inject } from 'vue'
-
 const { updateEdge, addEdges } = useVueFlow();
 
 function onConnect(params) {
@@ -14,12 +13,15 @@ function onEdgeUpdate({ edge, connection }) {
 }
 
 const TableStyle = {
+  display: 'flex',
   border: '1px solid #10b981',
-  background: '#ff251e',
+  background: '#ff3736',
   color: 'white',
   borderRadius: '5px',
   width: '200px',
-  height: '40px',
+  height: '50px',
+  alignItems: 'center',
+  justifyContent: 'space-between',
 }
 const RowStyle = {
   border: '1px solid #10b981',
@@ -27,12 +29,12 @@ const RowStyle = {
   color: '#000000',
   borderRadius: '0px',
   width: '200px',
-  height: '40px',
+  height: '50px',
 }
 const elements = ref([
   {
     id: '1',
-    type: 'toolbar',
+    type: 'table',
     label: 'First table',
     data: { toolbarPosition: Position.Top, toolbarVisible: true },
     position: { x: 0, y: -100 },
@@ -43,7 +45,7 @@ const elements = ref([
 const addTable = () => {
   elements.value.push({
     id: Math.random().toString(),
-    type: 'toolbar',
+    type: 'table',
     label: 'New Table',
     data: {
       toolbarPosition: Position.Top,
@@ -54,13 +56,13 @@ const addTable = () => {
 
   })
 }
+const updateLabel = (id, newLabel) => {
+  const element = elements.value.find(el => el.id === id);
+  if (element) {
+    element.label = newLabel;
+  }
+}
 
-provide('elements', elements)
-provide('addTable', addTable)
-
-
-
-let lastYPosition = 0;
 const addRow = (nodeProps) => {
 
   const existingRows = elements.value.filter(el => el.parentNode === nodeProps.id);
@@ -69,28 +71,28 @@ const addRow = (nodeProps) => {
     id: Date.now().toString(), // use a timestamp to generate a unique id
     type: 'row', // change this to your actual row node type
     label: 'New Row',
-    position: { x: position.x, y: position.y + 40 * (existingRows.length +1) }, // calculate y based on number of existing rows
+    position: { x: position.x, y: position.y + 50 * (existingRows.length +1) }, // calculate y based on number of existing rows
     style: RowStyle,
     draggable: false, //disable dragging for rows
     parentNode: nodeProps.id, //set the parent of the node (have it dragged with parent
     sourcePosition: 'right', // source handle is on the right
     targetPosition: 'left', // target handle is on the left
+    data: { editing: false, showModal: false, KeyMod: '', selectedSqlType: '' },
   }]
+
+
 }
 
 const deleteNode = (nodeId) => {
-  // Find the node to be deleted
+
   const nodeToDelete = elements.value.find(el => el.id === nodeId);
 
-  // If the deleted node is a parent, delete all its child nodes
-  if (nodeToDelete.type === 'toolbar') {
+  if (nodeToDelete.type === 'table') {
     elements.value = elements.value.filter(el => el.parentNode !== nodeId);
   }
 
-  // Filter out the node to be deleted
   elements.value = elements.value.filter(el => el.id !== nodeId);
 
-  // If the deleted node is a row, adjust the positions of other rows
   if (nodeToDelete.type === 'row') {
     const siblingRows = elements.value.filter(el => el.parentNode === nodeToDelete.parentNode && el.position.y > nodeToDelete.position.y);
     siblingRows.forEach((row, index) => {
@@ -98,11 +100,40 @@ const deleteNode = (nodeId) => {
     });
   }
 };
+const collectData = () => {
+  const links = elements.value.filter(el => el.type === 'smoothstep');
+  const nodes = elements.value.filter(elem => elem.type === 'table');
+  const data = nodes.map(node => {
+    const rows = elements.value.filter(row => row.parentNode === node.id);
+    return {
+      table: node.label,
+      rows: rows.map(row => {
+        return {id: row.id , label: row.label, KeyMod: row.data.KeyMod, sqlType: row.data.selectedSqlType, connectedTo: links.filter(link => link.target === row.id ).map(link => link.source)};
+      }),
+    }
 
+  });
+
+  console.log(data);
+
+}
+
+const updateKeyMod = (id, KeyMod) => {
+  const element = elements.value.find(el => el.id === id);
+  if (element) {
+    element.data.KeyMod = KeyMod;
+  }
+}
+provide('updateKeyMod', updateKeyMod);
+provide('updateLabel', updateLabel);
+provide('elements', elements)
+provide('addTable', addTable)
 
 </script>
 
 <template>
+  <button @mousedown.stop @click="collectData">Collect Data</button>
+
   <button @mousedown.stop @click="addTable">Add Table</button>
   <VueFlow
       :default-edge-options="{ type:'smoothstep' }"
@@ -114,23 +145,79 @@ const deleteNode = (nodeId) => {
   >
     <Background :variant="BackgroundVariant.Lines" />
 
-    <template #node-toolbar="{ id, data, label }">
-      <div style="display:flex; gap:.5rem; align-items:center;">
-        <button  @mousedown.stop @click="addRow({ id, data, label })">Add row</button>
-        {{ label }}
-        <button @mousedown.stop style="color: red; margin-left: auto;" @click="deleteNode(id)">x</button>
-      </div>
+    <template #node-table="{ id, data, label }">
+      <button class="table_button" @mousedown.stop @click="addRow({ id, data, label })">
+        <img class="table_icon" src="./components/icons/plus.svg" alt="Add row">
+      </button>
+
+      <input class="table_input" v-if="data.editing" :value="label" @input="updateLabel(id, $event.target.value)" @blur="data.editing = false">
+      <span class="table_input"  v-else @click="data.editing = true">{{ label }}</span>
+
+      <button class ="table_button" @mousedown.stop  @click="deleteNode(id)">
+        <img class="table_icon" src="./components/icons/cancel.svg" alt="Cancel">
+      </button>
     </template>
 
     <template #node-row="{ id, data, label }">
-      <div style="display:flex; gap:.5rem; align-items:center; ">
-        <Handle type="target" position="left" style="background: #555;" />
-        {{ label }}
-        <Handle type="source" position="right" style="background: #555;" />
-        <button @mousedown.stop style="color: red; margin-left: auto;" @click="deleteNode(id)">x</button>
-      </div>
-    </template>
+      <input class="row_input" v-if="data.editing" :value="label" @input="updateLabel(id, $event.target.value)" @blur="data.editing = false">
+      <span class="row_input" v-else @click="data.editing = true">{{ label }}</span>
+      <button class="table_button" @mousedown.stop @click="data.showModal = !data.showModal">
+        <img class="table_icon" src="./components/icons/key.svg" alt="Key">
+      </button>
 
+      <div class="modal" v-if="data.showModal">
+        <select v-model="data.KeyMod" @change="updateKeyMod(id, data.KeyMod)">
+          <option selected="selected" value="None">None</option>
+          <option value="Primary">Primary</option>
+          <option value="Unique">Unique</option>
+          <option value="Index">Index</option>
+        </select>
+        <button class="modal_close_button" @click="data.showModal = false">Close</button>
+      </div>
+
+      <select v-model="data.selectedSqlType">
+        <option value="">Select SQL Type</option>
+        <option value="INT">INT</option>
+        <option value="VARCHAR">VARCHAR</option>
+        <option value="TEXT">TEXT</option>
+        <option value="DATE">DATE</option>
+      </select>
+      <button class ="table_button" @mousedown.stop  @click="deleteNode(id)">
+        <img class="table_icon" src="./components/icons/cancel.svg" alt="Cancel">
+      </button>
+      <Handle type="source" position="right" />
+      <Handle type="target" position="left" />
+    </template>
 
   </VueFlow>
 </template>
+
+<style scoped>
+
+.vue-flow-basic-example {
+  width: 100%;
+  height: 100vh;
+  background-color: #f0f0f0;
+}
+.table_button {
+  width: 15%;
+  height: 70%;
+  padding: 0;
+  border: none;
+  background: none;
+}
+.table_icon {
+  width: 70%;
+  height: 70%;
+}
+.table_input{
+  width: 80%;
+}
+input {
+  padding: 0.5rem;
+  font-size: 1rem;
+  color: #000;
+  background-color: #fff;
+  border: none;
+}
+</style>
